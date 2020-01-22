ee7bf6b0481c37e111c4972e735cc36fb496590e
Samuel Paré-Chouinard <6parsam@gmail.com>
2020-01-22 10:34:39 -0500

    created rotate script

---
+++ CART315_exercises-master/Assets/rotatescript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class rotatescript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		this.transform.Rotate(0, 0.1f*Time.deltaTime, 0);
+
+        // instead of multiplying by Time.deltaTime, you can use FixedUpdate() to ensure a smoothe framerate for the update() function
+	}
+}
